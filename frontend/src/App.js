2c2ee5af767a508a028535bffe33e2dfd9c5ab0d
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2022-12-08 09:05:03 +0800

    frontend1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react"
 // import logo from "./logo.svg"
 // import "./App.css"
@@ -9,6 +10,11 @@ import Login from "./components/Login"
 import Register from "./containers/Register"
 import Calendar from "./containers/Calendar"
 import { AppBar } from "@mui/material"
+=======
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 42a5f36 (frontend1)
 
 const App = () => {
   return (
